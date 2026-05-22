cd151dfce29b4e035f3e51b6288e3f1c10539263
saleh <ntegesaleh@gmail.com>
2026-05-22 13:01:12 +0300

    intial changes index.html

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,15 @@
-document.addEventListener("DOMContentLoaded", 
-    () => { document.getElementById("currentyear").textContent = new Date().getFullYear();
-     document.getElementById("lastModified").textContent =
-        "Last Modified: " + document.lastModified;
-});
+document.addEventListener("DOMContentLoaded",
+    () => {
+        document.getElementById("currentyear").textContent = new Date().getFullYear();
+        document.getElementById("lastModified").textContent =
+            "Last Modified: " + document.lastModified;
+
+        const menuButton = document.querySelector("#menu");
+        const nav = document.querySelector("#nav");
+
+        menuButton.addEventListener("click", () => {
+            nav.classList.toggle("open");
+        });
+
+
+    });

14428f87dc5a43769608ee152634a18375bf9f69
saleh <ntegesaleh@gmail.com>
2026-06-05 02:06:31 +0300

    intail change

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -9,19 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    document.addEventListener("DOMContentLoaded", () => {
-    const lastModified = document.querySelector("#lastModified");
+    document.querySelector("#lastModified").textContent =
+        new Date(document.lastModified).toLocaleString();
 
-    if (lastModified) {
-        const fileTime = new Date(document.lastModified).toLocaleString();
-        const currentTime = new Date().toLocaleString();
-
-        lastModified.innerHTML = `
-            File Modified: ${fileTime}<br>
-            Current Time: ${currentTime}
-        `;
-    }
-});
 
     const timestamp = document.querySelector("#timestamp");
     if (timestamp) {

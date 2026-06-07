5e41e0876b9dc43ccb67e44712ba553c79f38541
saleh <ntegesaleh@gmail.com>
2026-06-07 17:24:30 +0300

    intial changes football site

--- football-site-plan/scripts/data.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-    const now = new Date();
-    document.getElementById("lastModified").textContent =
-        "Current Time: " + now.toLocaleString();
-});
